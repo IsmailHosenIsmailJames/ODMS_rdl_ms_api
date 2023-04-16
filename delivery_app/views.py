7f0f8b8833e9e014f5028f65d6fc1c5c2362864a
Tareq <tareq236@gmail.com>
2023-04-16 11:37:43 +0600

    m

--- delivery_app/views.py
+++ delivery_app/views.py
@@ -7,32 +7,61 @@ from rest_framework.response import Response
 def delivery_list(request,sap_id):
     if request.method == 'GET':
         d_type = request.query_params.get("type")
-        return Response({"success": True, "result": [{
-            'id': 1,
-            'invoice_date': '2023-04-11',
-            'customer_name': 'Hazi Liakot Ali Pharmacy',
-            'customer_addreess': 'Khejurbag (Near Pargandiara Bazar)',
-            'customer_mobile': '0171716732',
-            'delevery_type': 'Car',
-            'route_name': 'Shahbag',
-            'latitude': 23.7985906000000000,
-            'longitude': 90.4422262000000000,
-            'status': 'Pending',
-            'product_list':[
-                {
-                    'product_code':'1011123',
-                    'product_name':'Azicin Tablet 250gm',
-                    'qty': 10,
-                    'per_price': 20.50,
-                    'total_price': 205.00,
-                },
-                {
-                    'product_code':'1011124',
-                    'product_name':'Aristoplex Syrup 250ml',
-                    'qty': 40,
-                    'per_price': 60.32,
-                    'total_price': 2412.80,
-                }
-            ]
-            
-        }]}, status=status.HTTP_200_OK)
+        return Response({"success": True, "result": [
+            {
+                'id': 1,
+                'invoice_date': '2023-04-11',
+                'customer_name': 'Hazi Liakot Ali Pharmacy',
+                'customer_addreess': 'Khejurbag (Near Pargandiara Bazar)',
+                'customer_mobile': '0171716732',
+                'delevery_type': 'Car',
+                'route_name': 'Shahbag',
+                'latitude': 23.7985906000000000,
+                'longitude': 90.4422262000000000,
+                'status': 'Pending',
+                'product_list':[
+                    {
+                        'product_code':'1011123',
+                        'product_name':'Azicin Tablet 250gm',
+                        'qty': 10,
+                        'per_price': 20.50,
+                        'total_price': 205.00,
+                    },
+                    {
+                        'product_code':'1011124',
+                        'product_name':'Aristoplex Syrup 250ml',
+                        'qty': 40,
+                        'per_price': 60.32,
+                        'total_price': 2412.80,
+                    }
+                ]
+            },
+            {
+                'id': 1,
+                'invoice_date': '2023-04-11',
+                'customer_name': 'Hayder Pharmacy',
+                'customer_addreess': 'Khejurbag (Near Pargandiara Bazar)',
+                'customer_mobile': '0171716732',
+                'delevery_type': 'Car',
+                'route_name': 'Shahbag',
+                'latitude': 23.7985906000000000,
+                'longitude': 90.4422262000000000,
+                'status': 'Done',
+                'product_list':[
+                    {
+                        'product_code':'1011123',
+                        'product_name':'Azicin Tablet 250gm',
+                        'qty': 10,
+                        'per_price': 20.50,
+                        'total_price': 205.00,
+                    },
+                    {
+                        'product_code':'1011124',
+                        'product_name':'Aristoplex Syrup 250ml',
+                        'qty': 40,
+                        'per_price': 60.32,
+                        'total_price': 2412.80,
+                    }
+                ]
+            }
+            ]}, status=status.HTTP_200_OK)
