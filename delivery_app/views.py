b3c6f8095a632cb097e3226884f6d2babc9c5309
tareq236 <tareq236@gmail.com>
2023-06-04 13:58:17 +0600

    change date

--- delivery_app/views.py
+++ delivery_app/views.py
@@ -26,7 +26,7 @@ def delivery_list(request,sap_id):
                 "LEFT JOIN exf_customer_location cl ON sis.partner=cl.customer_id " \
                 "LEFT JOIN rdl_delivery d ON sis.billing_doc_no=d.billing_doc_no " \
                 "LEFT JOIN rdl_delivery_list dl ON sis.billing_doc_no=d.billing_doc_no " \
-                "WHERE dis.billing_date = '2023-03-28' AND dis.da_code = '%s';"
+                "WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s';"
 
     data_list = DeliveryInfoModel.objects.raw(sql,[sap_id])
     an_iterator = groupby(data_list, lambda x : x.billing_doc_no)
