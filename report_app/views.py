e488ddbcc1851b71920f013b54901c8d628a3f65
Tareq <tareq236@gmail.com>
2023-06-14 11:13:07 +0600

    m

--- report_app/views.py
+++ report_app/views.py
@@ -15,12 +15,11 @@ def execute_raw_query(query, params=None):
 def dashboard_report(request,sap_id):
     if request.method == 'GET':
         sql= "SELECT " \
-            "(SELECT COUNT(DISTINCT dis.billing_doc_no) c FROM rdl_delivery_info_sap dis WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_delivary," \
-            "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s') total_delivary_done, " \
-            "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND collection_status = 'Done') total_collection_done;"
+                "(SELECT COUNT(DISTINCT dis.billing_doc_no) c FROM rdl_delivery_info_sap dis WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_delivary," \
+                "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s') total_delivary_done, " \
+                "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND collection_status = 'Done') total_collection_done;"
         
         result = execute_raw_query(sql,[sap_id,sap_id,sap_id])
-        print(result[0][0])
 
         return Response({"success": True, "result": [{
             'delivery_remaining': result[0][0]-result[0][1],
