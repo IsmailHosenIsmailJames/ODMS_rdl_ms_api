31c9959fce27a2fd64cdf06a94e7380cea77dc62
Tareq <tareq236@gmail.com>
2023-06-14 11:23:07 +0600

    m

--- report_app/views.py
+++ report_app/views.py
@@ -17,7 +17,7 @@ def dashboard_report(request,sap_id):
         sql= "SELECT " \
                 "(SELECT COUNT(DISTINCT dis.billing_doc_no) c FROM rdl_delivery_info_sap dis WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_delivary," \
                 "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s') total_delivary_done, " \
-                "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND collection_status = 'Done') total_collection_done;"
+                "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND cash_collection_status = 'Done') total_collection_done;"
         
         result = execute_raw_query(sql,[sap_id,sap_id,sap_id])
 
