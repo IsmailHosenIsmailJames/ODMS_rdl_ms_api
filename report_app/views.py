b31ced5787b5e7dd612984550b1bbafe432dcf76
tareq <tareq236@gmail.com>
2023-08-10 17:18:11 +0600

    modify

--- report_app/views.py
+++ report_app/views.py
@@ -16,15 +16,16 @@ def dashboard_report(request,sap_id):
     if request.method == 'GET':
         sql= "SELECT " \
 	            "(SELECT COUNT(DISTINCT dis.billing_doc_no) c FROM rdl_delivery_info_sap dis WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_delivary," \
-	            "(SELECT SUM(sis.net_val) c FROM rdl_delivery_info_sap dis INNER JOIN rpl_sales_info_sap sis ON dis.billing_doc_no=sis.billing_doc_no WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_gate_pass_amount," \
 	            "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s') total_delivary_done, " \
 	            "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND delivery_status = 'Done' AND cash_collection_status IS NULL) total_collection, " \
+                "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND cash_collection_status = 'Done') total_collection_done, " \
+                "(SELECT SUM(sis.net_val) c FROM rdl_delivery_info_sap dis INNER JOIN rpl_sales_info_sap sis ON dis.billing_doc_no=sis.billing_doc_no WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_gate_pass_amount," \
 	            "(SELECT SUM(d.cash_collection) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND delivery_status = 'Done' AND cash_collection_status = 'Done') total_collection_amount," \
 	            "(SELECT SUM(dl.return_net_val) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_net_val IS NOT NULL) total_return_amount," \
-	            "(SELECT SUM(dl.return_quantity) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_quantity IS NOT NULL) total_return_quantity," \
-	            "(SELECT COUNT(*) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND cash_collection_status = 'Done') total_collection_done;"
+	            "(SELECT SUM(dl.return_quantity) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_quantity IS NOT NULL) total_return_quantity;" \
+	            
         
-        result = execute_raw_query(sql,[sap_id,sap_id,sap_id,sap_id])
+        result = execute_raw_query(sql,[sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id])
 
         return Response({"success": True, "result": [{
             'delivery_remaining': result[0][0]-result[0][1],
@@ -32,10 +33,10 @@ def dashboard_report(request,sap_id):
             'cash_remaining': result[0][2],
             'cash_done': result[0][3],
             'sap_id': sap_id,
-            'total_gate_pass_amount': 0,
-            'total_collection_amount': 0, 
-            'total_return_amount': 0, 
-            'total_return_quantity': 0,
+            'total_gate_pass_amount': result[0][4],
+            'total_collection_amount': result[0][5], 
+            'total_return_amount': result[0][6], 
+            'total_return_quantity': result[0][7],
             'due_amount_total': 0,
             'previous_day_due': 0
         }]}, status=status.HTTP_200_OK)
