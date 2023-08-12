40fdca3808b4a28f2cab389f3c07735be094ff7b
tareq <tareq236@gmail.com>
2023-08-12 12:16:21 +0600

    modify

--- report_app/views.py
+++ report_app/views.py
@@ -22,10 +22,11 @@ def dashboard_report(request,sap_id):
                 "(SELECT SUM(sis.net_val) c FROM rdl_delivery_info_sap dis INNER JOIN rpl_sales_info_sap sis ON dis.billing_doc_no=sis.billing_doc_no WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s') total_gate_pass_amount," \
 	            "(SELECT SUM(d.cash_collection) c FROM rdl_delivery d WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND delivery_status = 'Done' AND cash_collection_status = 'Done') total_collection_amount," \
 	            "(SELECT SUM(dl.return_net_val) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_net_val IS NOT NULL) total_return_amount," \
-	            "(SELECT SUM(dl.return_quantity) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_quantity IS NOT NULL) total_return_quantity;" \
+	            "(SELECT SUM(dl.return_quantity) c FROM rdl_delivery d INNER JOIN rdl_delivery_list dl ON d.id=dl.delivery_id WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done' AND dl.return_quantity IS NOT NULL) total_return_quantity," \
+                "(SELECT SUM(sis.net_val) net_val, SUM(DISTINCT d.cash_collection) cash_collection, (SUM(sis.net_val) - SUM(DISTINCT d.cash_collection)) due FROM rdl_delivery d INNER JOIN rpl_sales_info_sap sis ON d.billing_doc_no=sis.billing_doc_no WHERE d.billing_date = CURRENT_DATE() AND d.da_code = '%s' AND d.delivery_status = 'Done' AND d.cash_collection_status = 'Done') total_due;"
 	            
         
-        result = execute_raw_query(sql,[sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id])
+        result = execute_raw_query(sql,[sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id,sap_id])
 
         return Response({"success": True, "result": [{
             'delivery_remaining': result[0][0]-result[0][1],
@@ -37,7 +38,7 @@ def dashboard_report(request,sap_id):
             'total_collection_amount': result[0][5], 
             'total_return_amount': result[0][6], 
             'total_return_quantity': result[0][7],
-            'due_amount_total': 0,
+            'due_amount_total': result[0][7],
             'previous_day_due': 0
         }]}, status=status.HTTP_200_OK)
     
