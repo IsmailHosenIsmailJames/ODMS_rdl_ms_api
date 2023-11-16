2ba638e5701f0280edd22031b5ee57d1c7d8b340
tareq <tareq236@gmail.com>
2023-11-16 22:31:02 +0600

    modify

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,6 +4,6 @@ from user_app import views
 urlpatterns = [
     path('admin_login', views.user_admin_login),
     path('user_login', views.user_login),
-    path('user_details', views.user_login),
+    path('user_details', views.user_details),
     path('user_registration', views.user_registration),
 ]
