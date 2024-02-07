c429e3d196e2f375570b08ea05af20ff9c5feefd
tareq <tareq236@gmail.com>
2024-02-07 08:56:46 +0600

    modify

--- collection_app/views.py
+++ collection_app/views.py
@@ -53,93 +53,96 @@ def cash_collection_list_v2(request,sap_id):
                 "WHERE dis.billing_date = CURRENT_DATE() AND dis.da_code = '%s' "+query+" ;"
         
         data_list = DeliveryInfoModel.objects.raw(sql,[sap_id])
-        an_iterator = groupby(data_list, lambda x : x.billing_doc_no)
-        data = []
-        for key, group in an_iterator:
-            key_and_group = {key : list(group)}
-            sub_data = []
-            for item in key_and_group[key]:
-                rec_qty = 0
-                if item.delivery_quantity is not None:
-                    rec_qty = item.delivery_quantity
-                rec_net_val = 0
-                if item.delivery_net_val is not None:
-                    rec_net_val = item.delivery_net_val
-                return_quantity = 0
-                if item.return_quantity is not None:
-                    return_quantity = item.return_quantity
-                return_net_val = 0
-                if item.return_net_val is not None:
-                    return_net_val = item.return_net_val
+        if len(data_list) == 0:
+            return Response({"success": False, "message": "Data not available!"}, status=status.HTTP_200_OK)
+        else:
+            an_iterator = groupby(data_list, lambda x : x.billing_doc_no)
+            data = []
+            for key, group in an_iterator:
+                key_and_group = {key : list(group)}
+                sub_data = []
+                for item in key_and_group[key]:
+                    rec_qty = 0
+                    if item.delivery_quantity is not None:
+                        rec_qty = item.delivery_quantity
+                    rec_net_val = 0
+                    if item.delivery_net_val is not None:
+                        rec_net_val = item.delivery_net_val
+                    return_quantity = 0
+                    if item.return_quantity is not None:
+                        return_quantity = item.return_quantity
+                    return_net_val = 0
+                    if item.return_net_val is not None:
+                        return_net_val = item.return_net_val
 
-                sub_data.append({
-                    "id": item.list_id,
-                    "matnr": item.matnr,
-                    "quantity": item.quantity,
-                    "tp": item.tp,
-                    "vat": item.vat,
-                    "net_val": item.net_val,
-                    "batch": item.batch,
-                    "material_name": item.material_name,
-                    "brand_description": item.brand_description,
-                    "brand_name": item.brand_name,
-                    "delivery_quantity": rec_qty,
-                    "delivery_net_val": rec_net_val,
-                    "return_quantity": return_quantity,
-                    "return_net_val": return_net_val,
+                    sub_data.append({
+                        "id": item.list_id,
+                        "matnr": item.matnr,
+                        "quantity": item.quantity,
+                        "tp": item.tp,
+                        "vat": item.vat,
+                        "net_val": item.net_val,
+                        "batch": item.batch,
+                        "material_name": item.material_name,
+                        "brand_description": item.brand_description,
+                        "brand_name": item.brand_name,
+                        "delivery_quantity": rec_qty,
+                        "delivery_net_val": rec_net_val,
+                        "return_quantity": return_quantity,
+                        "return_net_val": return_net_val,
+                    })
+
+                    cash_collection = 0
+                    if key_and_group[key][0].cash_collection is not None:
+                        cash_collection = key_and_group[key][0].cash_collection
+
+                    
+                main_data = {
+                    "id": key_and_group[key][0].id,
+                    "billing_doc_no": key_and_group[key][0].billing_doc_no,
+                    "billing_date": key_and_group[key][0].billing_date,
+                    "route_code": key_and_group[key][0].route,
+                    "route_name": key_and_group[key][0].route_name,
+                    "da_code": key_and_group[key][0].da_code,
+                    "da_name": key_and_group[key][0].da_name,
+                    "partner": key_and_group[key][0].partner,
+                    "customer_name": key_and_group[key][0].customer_name,
+                    "customer_address": key_and_group[key][0].customer_address,
+                    "customer_mobile": key_and_group[key][0].customer_mobile,
+                    "latitude": key_and_group[key][0].latitude,
+                    "longitude": key_and_group[key][0].longitude,
+                    "delivery_status": key_and_group[key][0].delivery_status,
+                    "cash_collection": cash_collection,
+                    "cash_collection_status": key_and_group[key][0].cash_collection_status,
+                    "gate_pass_no": key_and_group[key][0].gate_pass_no,
+                    "vehicle_no": key_and_group[key][0].vehicle_no,
+                    "transport_type": key_and_group[key][0].transport_type,
+                    "product_list": sub_data
+                }
+                data.append(main_data)
+
+                key_func = itemgetter('billing_date', 'partner')
+                sorted_data = sorted(data, key=key_func)
+                grouped_data = {key: list(group) for key, group in groupby(sorted_data, key=key_func)}
+                customer_data = []
+                for (billing_date, partner), group in grouped_data.items():
+                    customer_data.append({
+                        "billing_date": group[0]['billing_date'],
+                        "route_code": group[0]['route_code'],
+                        "route_name": group[0]['route_name'],
+                        "da_code": group[0]['da_code'],
+                        "da_name": group[0]['da_name'],
+                        "partner": group[0]['partner'],
+                        "customer_name": group[0]['customer_name'],
+                        "customer_address": group[0]['customer_address'],
+                        "customer_mobile": group[0]['customer_mobile'],
+                        "gate_pass_no": group[0]['gate_pass_no'],
+                        "latitude": group[0]['latitude'],
+                        "longitude": group[0]['longitude'],
+                        "invoice_list": group,
                 })
 
-                cash_collection = 0
-                if key_and_group[key][0].cash_collection is not None:
-                    cash_collection = key_and_group[key][0].cash_collection
-
-                
-            main_data = {
-                "id": key_and_group[key][0].id,
-                "billing_doc_no": key_and_group[key][0].billing_doc_no,
-                "billing_date": key_and_group[key][0].billing_date,
-                "route_code": key_and_group[key][0].route,
-                "route_name": key_and_group[key][0].route_name,
-                "da_code": key_and_group[key][0].da_code,
-                "da_name": key_and_group[key][0].da_name,
-                "partner": key_and_group[key][0].partner,
-                "customer_name": key_and_group[key][0].customer_name,
-                "customer_address": key_and_group[key][0].customer_address,
-                "customer_mobile": key_and_group[key][0].customer_mobile,
-                "latitude": key_and_group[key][0].latitude,
-                "longitude": key_and_group[key][0].longitude,
-                "delivery_status": key_and_group[key][0].delivery_status,
-                "cash_collection": cash_collection,
-                "cash_collection_status": key_and_group[key][0].cash_collection_status,
-                "gate_pass_no": key_and_group[key][0].gate_pass_no,
-                "vehicle_no": key_and_group[key][0].vehicle_no,
-                "transport_type": key_and_group[key][0].transport_type,
-                "product_list": sub_data
-            }
-            data.append(main_data)
-
-            key_func = itemgetter('billing_date', 'partner')
-            sorted_data = sorted(data, key=key_func)
-            grouped_data = {key: list(group) for key, group in groupby(sorted_data, key=key_func)}
-            customer_data = []
-            for (billing_date, partner), group in grouped_data.items():
-                customer_data.append({
-                    "billing_date": group[0]['billing_date'],
-                    "route_code": group[0]['route_code'],
-                    "route_name": group[0]['route_name'],
-                    "da_code": group[0]['da_code'],
-                    "da_name": group[0]['da_name'],
-                    "partner": group[0]['partner'],
-                    "customer_name": group[0]['customer_name'],
-                    "customer_address": group[0]['customer_address'],
-                    "customer_mobile": group[0]['customer_mobile'],
-                    "gate_pass_no": group[0]['gate_pass_no'],
-                    "latitude": group[0]['latitude'],
-                    "longitude": group[0]['longitude'],
-                    "invoice_list": group,
-            })
-
-    return Response({"success": True, "result": customer_data}, status=status.HTTP_200_OK)
+        return Response({"success": True, "result": customer_data}, status=status.HTTP_200_OK)
     
 
 @api_view(['GET'])
