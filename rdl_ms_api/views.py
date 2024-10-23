6ab0d1c37a8998f9a434327efc02abf45fa8ec29
najmulislamnajim <ninbd17@gmail.com>
2024-10-23 21:26:18 +0600

    [UPDATE] update app download link

--- rdl_ms_api/views.py
+++ rdl_ms_api/views.py
@@ -12,19 +12,19 @@ def app_info(request):
             "removeCacheOnUpdate":False,
             "removeDataOnUpdate":False,
             "removeCacheAndDataOnUpdate":False,
-            "downloadLink":"https://github.com/odms/odms/releases/tag/v0.0.1",
+            "downloadLink":"https://github.com/IsmailHosenIsmailJames/ODMS_app_rdl_radiant/releases/download/v1.7.3/app-release.apk",
             "downloadLinkList":[
                 {
                     "architecture":"x86_64",
-                    "link":"https://github.com/odms/odms/releases/tag/v0.0.1"
+                    "link":"https://github.com/IsmailHosenIsmailJames/ODMS_app_rdl_radiant/releases/download/v1.7.3/app-x86_64-release.apk"
                 },
                 {
                     "architecture":"armeabi-v7a",
-                    "link":"https://github.com/odms/odms/releases/tag/v0.0.1"
+                    "link":"https://github.com/IsmailHosenIsmailJames/ODMS_app_rdl_radiant/releases/download/v1.7.3/app-armeabi-v7a-release.apk"
                 },
                 {
                     "architecture":"arm64-v8a",
-                    "link":"https://github.com/odms/odms/releases/tag/v0.0.1"
+                    "link":"https://github.com/IsmailHosenIsmailJames/ODMS_app_rdl_radiant/releases/download/v1.7.3/app-arm64-v8a-release.apk"
                 }
             ]
         }
