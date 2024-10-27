f51aef14c273b38680b751544aedbbf5e5827977
najmulislamnajim <ninbd17@gmail.com>
2024-10-27 09:44:01 +0600

    [FIX] fix delivery remaining list bug

--- delivery_app/views.py
+++ delivery_app/views.py
@@ -39,13 +39,13 @@ def delivery_list_v2(request,sap_id):
                 "INNER JOIN rpl_sales_info_sap sis ON dis.billing_doc_no=sis.billing_doc_no " \
                 "INNER JOIN rpl_material m ON sis.matnr=m.matnr " \
                 "INNER JOIN rpl_customer c ON sis.partner=c.partner " \
-                "INNER JOIN rdl_customer_location rcl ON c.partner=rcl.customer_id " \
+                "LEFT JOIN rdl_customer_location rcl ON c.partner=rcl.customer_id " \
                 "LEFT JOIN (SELECT DISTINCT customer_id, latitude, longitude FROM rdl_customer_location LIMIT 1) cl ON sis.partner = cl.customer_id " \
                 "LEFT JOIN rdl_delivery d ON sis.billing_doc_no=d.billing_doc_no " \
                 "LEFT JOIN rdl_delivery_list dl ON d.id=dl.delivery_id AND sis.matnr=dl.matnr AND sis.batch=dl.batch " \
                 "WHERE dis.da_code = '%s' "+query+" ;"
     
-    # print(sql)
+    print(sql,sap_id)
     data_list = DeliveryInfoModel.objects.raw(sql,[sap_id])
     if len(data_list) == 0:
         return Response({"success": False, "message": "Data not available!"}, status=status.HTTP_200_OK)
